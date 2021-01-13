d5814252365738dbd18f094cb33e46d837bae635
안현모 <blue_pope@hotmail.com>
2021-01-13 23:43:34 +0900

    2강 완료!

---
+++ Study02/Converters/HeaderToImageConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace Study02.Converters
+{
+    [ValueConversion(typeof(string),typeof(BitmapImage))]
+    public class HeaderToImageConverter : IValueConverter
+    {
+        public static HeaderToImageConverter Instance = new HeaderToImageConverter();
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var path = value as string;
+
+            if (path == null)
+                return null;
+
+            if (File.Exists(path))
+            {
+                return new BitmapImage(new Uri($"pack://application:,,,/Images/file.png"));
+            }
+            else if (Directory.Exists(path))
+            {
+                return new BitmapImage(new Uri($"pack://application:,,,/Images/folder.png"));
+            }
+            else
+            {
+                return new BitmapImage(new Uri($"pack://application:,,,/Images/drive.png"));
+            }
+            
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Study02/MainWindow.xaml.cs
+++ Study02/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Study02
 {
@@ -23,6 +15,70 @@ namespace Study02
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (var drive in Directory.GetLogicalDrives())
+            {
+                var item = new TreeViewItem();
+
+                item.Header = drive;
+                item.Tag = drive;
+
+                item.Items.Add(null);
+                item.Expanded += Folder_Expanded;
+
+                FolderView.Items.Add(item);
+            }
+        }
+
+        private void Folder_Expanded(object sender, RoutedEventArgs e)
+        {
+            var item = (TreeViewItem)sender;
+
+            if (item.Items.Count != 1 || item.Items[0] != null)
+                return;
+
+            item.Items.Clear();
+
+            string fullPath = (string)item.Tag;
+
+            var dirList = new List<string>();
+            try
+            {
+                string[] dirs = Directory.GetDirectories(fullPath);
+
+                if (dirs.Length > 0)
+                    dirList.AddRange(dirs);
+
+                dirList.ForEach((dir) =>
+                {
+                    var subItem = new TreeViewItem();
+                    subItem.Header = Path.GetFileName(dir);
+                    subItem.Tag = dir;
+
+                    subItem.Items.Add(null);
+                    subItem.Expanded += Folder_Expanded;
+
+                    item.Items.Add(subItem);
+                });
+
+                string[] files = Directory.GetFiles(fullPath);
+                
+                if (files.Length > 0)
+                    dirList.AddRange(files);
+
+                foreach(string filePath in files)
+                {
+                    var subItem = new TreeViewItem();
+                    subItem.Header = Path.GetFileName(filePath);
+                    subItem.Tag = filePath;
+
+                    item.Items.Add(subItem);
+                }
+            }
+            catch { }
         }
     }
 }
